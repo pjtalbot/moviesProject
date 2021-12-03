432c59667767d4000054b69ba99f8a7196961e5f
Paul Jordan Talbot <pauljordantalbot@gmail.com>
2021-12-03 12:08:55 -0500

    add to list functionality added

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 let moviesList = [];
 
+let currentId = 0;
+
 function createMovieDataHTML(data) {
 	return `
       <tr>
@@ -18,14 +20,27 @@ $(function() {
 	$('#movie-submit-form').on('submit', function(e) {
 		e.preventDefault();
 		let title = $('#title').val();
-		let rating = $('#rating').val;
+		let rating = $('#rating').val();
 
 		let newData = { title, rating, currentId };
 		const divToAdd = createMovieDataHTML(newData);
 
-		$('movie-table-body').append(divToAdd);
+		currentId++;
+		moviesList.push(newData);
+
+		$('#movie-table-body').append(divToAdd);
 
 		// research this ".trigger" method, borrowed from solution, not fully understood
-		$('new-movie-form').trigger('reset');
+		$('movie-submit-form').trigger('reset');
+	});
+
+	$('tbody').on('click', '.btn.btn-danger', function(e) {
+		let removeIndex = moviesList.findIndex(function(movie) {
+			return movie.currentId === +$(e.target).data('deleteId');
+		});
+
+		moviesList.splice(removeIndex, 1);
+
+		$(e.target).parent.remove();
 	});
 });
