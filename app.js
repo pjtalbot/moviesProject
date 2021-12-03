a919b5fdfb6f82da6a1d89a3c60ec350fcfbd05a
Paul Jordan Talbot <pauljordantalbot@gmail.com>
2021-12-03 16:25:11 -0500

    fully working demo, first draft

--- app.js
+++ app.js
@@ -2,18 +2,23 @@ let moviesList = [];
 
 let currentId = 0;
 
-function createMovieDataHTML(data) {
+function createMovieDataHTML(inputData) {
 	return `
       <tr>
-        <td>${data.title}</td>
-        <td>${data.rating}</td>
+        <td>${inputData.title}</td>
+        <td>${inputData.rating}</td>
         <td>
-          <button class="btn btn-danger" data-delete-id=${data.currentId}>
+          <button class="btn btn-danger" data-delete-id=${inputData.currentId}>
             Delete
           </button>
         </td>
       <tr>
     `;
+}
+
+function updateDataList(data) {
+	currentId++;
+	moviesList.push(data);
 }
 
 $(function() {
@@ -25,8 +30,7 @@ $(function() {
 		let newData = { title, rating, currentId };
 		const divToAdd = createMovieDataHTML(newData);
 
-		currentId++;
-		moviesList.push(newData);
+		updateDataList(newData);
 
 		$('#movie-table-body').append(divToAdd);
 
@@ -41,6 +45,38 @@ $(function() {
 
 		moviesList.splice(removeIndex, 1);
 
-		$(e.target).parent.remove();
+		$(e.target).parents('tr').remove();
+	});
+
+	$('.fas').on('click', function(e) {
+		let direction = $(e.target).hasClass('fa-sort-down') ? 'down' : 'up';
+		let sortKey = $(e.target).attr('id');
+		let sortedMovies = sortBy(moviesList, sortKey, direction);
+
+		$('#movie-table-body').empty();
+
+		for (let m of sortedMovies) {
+			const divToAdd = createMovieDataHTML(m);
+			$('#movie-table-body').append(divToAdd);
+		}
+
+		$(e.target).toggleClass('fa-sort-down');
+		$(e.target).toggleClass('fa-sort-up');
 	});
 });
+
+function sortBy(array, sortKey, direction) {
+	return array.sort(function(a, b) {
+		if (sortKey === 'rating') {
+			a[sortKey] = +a[sortKey];
+			b[sortKey] = +b[sortKey];
+		}
+		if (a[sortKey] > b[sortKey]) {
+			return direction === 'up' ? 1 : -1;
+		} else if (b[sortKey] > a[sortKey]) {
+			return direction === 'up' ? -1 : 1;
+		} else {
+			return 0;
+		}
+	});
+}
